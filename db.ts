6e879d1c4cf6fe4fbb8a35540fa57e3d7bf456b1
Victor Ross <victor2ross@gmail.com>
2023-11-23 15:03:05 -0300

    Production database

--- db.ts
+++ db.ts
@@ -4,15 +4,24 @@ import { Client, QueryResult } from 'pg';
 loadEnvConfig(process.cwd());
 
 export async function getClient(): Promise<Client> {
-  const client = new Client({
-    host: process.env.NEXT_PUBLIC_POSTGRES_HOST,
-    port: Number(process.env.NEXT_PUBLIC_POSTGRES_PORT),
-    user: process.env.NEXT_PUBLIC_POSTGRES_USER,
-    password: process.env.NEXT_PUBLIC_POSTGRES_PASSWORD,
-    database: process.env.NEXT_PUBLIC_POSTGRES_DATABASE,
-  });
+  // Production
+  if (process.env.POSTGRES_URL) {
+    const client = new Client({
+      connectionString: process.env.POSTGRES_URL + '?sslmode=require',
+    });
+    return client;
+  } else {
+    // Development
+    const client = new Client({
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+    });
 
-  return client;
+    return client;
+  }
 }
 
 export async function sql(
